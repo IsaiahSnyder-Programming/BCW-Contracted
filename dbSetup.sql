9d10a194ddd2ad18b27218fb5b6aff0030d2193e
Isaiah Snyder <isaiahsnyder.programming@gmail.com>
2022-04-12 16:55:59 -0600

    Nuked my SQL

--- dbSetup.sql
+++ dbSetup.sql
@@ -8,40 +8,54 @@ CREATE TABLE IF NOT EXISTS accounts(
 ) default charset utf8 COMMENT '';
 --
 --
-CREATE TABLE IF NOT EXISTS companies(
-  creatorId varchar(255) NOT NULL,
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  name VARCHAR(255) NOT NULL,
-  location VARCHAR(255) NOT NULL,
-  FOREIGN KEY (creatorId) REFERENCES accounts(id)
-) default charset utf8 COMMENT '';
+UPDATE
+  accounts
+SET
+  id = "6255f845dbac55a6d8a432c7"
+WHERE
+  id = "621fe5d6dbe50cea2b338f0c";
 --
+  --
+SELECT
+  *
+FROM
+  accounts;
 --
-DROP TABLE companies;
+  --
+  CREATE TABLE IF NOT EXISTS companies(
+    creatorId varchar(255) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creatorId) REFERENCES accounts(id)
+  ) default charset utf8 COMMENT '';
 --
+  --
+  DROP TABLE companies;
 --
+  --
 INSERT INTO
   companies(name, location, `creatorId`)
 VALUES(
     "Tam & Hams Green Eggs and Jam",
     "Yeet Street",
-    "621fe5d6dbe50cea2b338f0c"
+    "6255f845dbac55a6d8a432c7"
   );
 INSERT INTO
   companies(name, location, `creatorId`)
 VALUES(
     "Tyler Scotts Brewery and Bar",
     "Yeet Street",
-    "621fe5d6dbe50cea2b338f0c"
+    "6255f845dbac55a6d8a432c7"
   );
 INSERT INTO
   companies(name, location, `creatorId`)
 VALUES(
     "Drakes Donkeys",
     "Yeet Street",
-    "621fe5d6dbe50cea2b338f0c"
+    "6255f845dbac55a6d8a432c7"
   );
 --
   --
@@ -71,9 +85,9 @@ INSERT INTO
   companies (name, location, creatorId)
 VALUES
   (
-    "The Place",
+    "Tam and Ham Green Eggs and Jam",
     "Yeet Street",
-    "621fe5d6dbe50cea2b338f0c"
+    "6255f845dbac55a6d8a432c7"
   );
 SELECT
   LAST_INSERT_ID();
@@ -98,19 +112,19 @@ SELECT
   --
 INSERT INTO
   contractors(name, creatorId)
-VALUES("Drake", "621fe5d6dbe50cea2b338f0c");
+VALUES("Drake", "6255f845dbac55a6d8a432c7");
 INSERT INTO
   contractors(name, creatorId)
-VALUES("Tamra", "621fe5d6dbe50cea2b338f0c");
+VALUES("Tamra", "6255f845dbac55a6d8a432c7");
 INSERT INTO
   contractors(name, creatorId)
-VALUES("Hannah", "621fe5d6dbe50cea2b338f0c");
+VALUES("Hannah", "6255f845dbac55a6d8a432c7");
 INSERT INTO
   contractors(name, creatorId)
-VALUES("Ty", "621fe5d6dbe50cea2b338f0c");
+VALUES("Ty", "6255f845dbac55a6d8a432c7");
 INSERT INTO
   contractors(name, creatorId)
-VALUES("Scott", "621fe5d6dbe50cea2b338f0c");
+VALUES("Harrison", "6255f845dbac55a6d8a432c7");
 --
   --
 SELECT
